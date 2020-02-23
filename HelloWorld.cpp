81c33dca8a19234e391eb90c2c5da31d36ed4fa5
verlitas <55808608+verlitas@users.noreply.github.com>
2020-02-23 10:20:23 -0700

    first commit with hello world

---
+++ HelloWorld.cpp
@@ -0,0 +1,12 @@
+// pre-processor directive tells the computer to grab this library
+#include <iostream>
+using namespace std; //get basic commands from standard library
+
+int main() //main entry to the program
+{
+    cout << "Hello World \n";
+    // cout << "I haven't written in C++ in a year" << endl;
+    cout << "So this \t is me trying to \t write some basic  \t code from what I remember";
+    system("pause");
+    return 0;
+}
